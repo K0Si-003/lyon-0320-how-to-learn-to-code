38d43e59df634ff1c1573e02d51f265e46c5772c
Kosi <hugo.pioda@gmail.com>
2020-04-02 14:43:21 +0200

    Add back to top button

--- js/back-top-btn.js
+++ js/back-top-btn.js
@@ -1,32 +1,20 @@
-$(document).ready(function () {
-  $('a').on('click', function (event) {
-    if (this.hash !== '') {
-      event.preventDefault();
+const mybutton = document.getElementById('myBtn');
 
-      var hash = this.hash;
+window.onscroll = function () { scrollFunction(); };
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function () {
-        window.location.hash = hash;
-      });
-    }
-  });
-});
+function scrollFunction () {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = 'block';
+  } else {
+    mybutton.style.display = 'none';
+  }
+}
 
-jQuery(document).ready(function () {
-  var btn = $('#button');
-
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 300) {
-      btn.addClass('show');
-    } else {
-      btn.removeClass('show');
-    }
-  });
-
-  btn.on('click', function (event) {
-    event.preventDefault();
-    $('html, body').animate({ scrollTop: 0 }, '300');
-  });
-});
+function topFunction () {
+  const elementScrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+  if (elementScrollTop > 500) {
+    window.requestAnimationFrame(topFunction);
+    window.scrollTo(0, elementScrollTop - elementScrollTop / 15);
+  }
+}
+topFunction();
